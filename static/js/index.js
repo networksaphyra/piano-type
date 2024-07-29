3735464e75022a49b4bdbb8fe8b72b1f9e95640d
networksaphyra <networksaphyra004@gmail.com>
2024-07-29 17:08:00 -0400

    feat: parsed all characters, will allow to create cursor in the future and allows more control.

--- static/js/index.js
+++ static/js/index.js
@@ -13,11 +13,29 @@ async function main() {
 
   function generateText(wordCount) {
     let wordList = [];
-    for (let count = 1; count <= wordCount; ++count) {
+    for (let word = 0; word < wordCount; ++word) {
       let index = Math.floor(Math.random() * words.length);
-      wordList.push(words[index]);
+      const wordString = `${word}`;
+      let currentWord = createWordTag(wordString);
+
+      for (let pos = 0; pos < words[index].length; ++pos) {
+        const position = `word-${word}-position-${pos}`;
+        const character = words[index][pos];
+
+        const pElement = createLetterTag(character, position);
+        currentWord.append(pElement);
+      }
+      wordList.push(currentWord);
     }
-    return wordList.join(" ");
+    return wordList;
+  }
+
+  function createWordTag(word) {
+    return $("<span></span>").addClass("word").attr("id", word);
+  }
+
+  function createLetterTag(character, position) {
+    return $("<span></span>").text(character).addClass("letter").attr("id", position);
   }
 
   await fetchText();
@@ -26,9 +44,19 @@ async function main() {
 
   function displayText() {
     console.log("BRO I ENTERED THE METHOD ON GOD");
-    let generatedText = generateText(20);
+    let generatedText = generateText(60);
     console.log(generatedText);
-    typingArea.text(generatedText);
+    const textContainer = $("#textContainer");
+    textContainer.empty();
+
+    generatedText.forEach((word, index) => {
+      textContainer.append(word);
+      if (index < generatedText.length - 1) textContainer.append(" ");
+    });
   }
 
+  function autoScroll() {
+    const typingArea = document.getElementById('typingArea');
+    typingArea.scrollTop = typingArea.scrollHeight;
+  }
 }
